7441b6d1698a725231c045d6a18e466d9aacd590
OmSoma1912 <61110267+OmSoma1912@users.noreply.github.com>
2020-07-09 17:25:56 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,46 @@
+class Form{
+    constructor(){
+        this.title = createElement('h2');
+        this.input = createInput("Name");
+        this.button = createButton('Play');
+        this.greeting = createElement('h3');
+        this.reset = createButton('Reset');
+    }
+    
+    hide(){
+        this.greeting.hide();
+        this.button.hide();
+        this.input.hide();
+        this.title.hide();
+    }
+    
+    display(){
+        
+        this.title.html("Need for Speed Heat");
+        this.title.position(displayWidth/2 - 50,0);
+
+        this.input.position(displayWidth/2 - 50, displayHeight/2 - 20);
+        this.button.position(displayWidth/2 - 30, displayHeight/2);
+        this.reset.position(displayWidth - 100,20);
+
+        this.button.mousePressed(()=>{
+            this.input.hide();
+            this.button.hide();
+
+            player.name = this.input.value();
+
+            playerCount+=1;
+            player.index = playerCount
+            player.update(name);
+            player.updateCount(playerCount);
+            
+            this.greeting.html("Welcome " + player.name);
+            this.greeting.position(displayWidth - 70,displayHeight/2 - 30);
+        });
+
+        this.reset.mousePressed(()=>{
+            player.updateCount(0);
+            game.update(0);
+        });
+    }
+}
